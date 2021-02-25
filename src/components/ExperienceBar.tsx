0642e3d2cc229a7f6c437fd0dc1aac17095b948b
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-02-25 10:46:42 -0300

    Finish Lesson/Day 03 - Context and Components

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,29 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import css from '../css/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(
+    ChallengesContext,
+  );
+
+  const percentToNextLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel;
+
   return (
     <header className={css.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${percentToNextLevel}` }} />
 
-        <span className={css.currentExperience} style={{ left: '50%' }}>
-          300 xp
+        <span
+          className={css.currentExperience}
+          style={{ left: `${percentToNextLevel}` }}
+        >
+          {currentExperience} xp
         </span>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
